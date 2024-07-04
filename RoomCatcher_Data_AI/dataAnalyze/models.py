afc3d3f34c496167c56a69cdbd0233ea2356ec93
5hseok <ohhs1010@gmail.com>
2024-07-05 00:59:08 +0900

    ✨ [FEAT] Product Model 정의

--- RoomCatcher_Data_AI/dataAnalyze/models.py
+++ RoomCatcher_Data_AI/dataAnalyze/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    id = models.IntegerField(primary_key=True)
+    productName = models.CharField(max_length=30)
+    productRoomType = models.CharField(max_length=30)
+    productSellingType = models.CharField(max_length=30)
+    productPrice = models.CharField(max_length=30)
+    productAddr = models.CharField(max_length=50)
+    productInfo = models.CharField(max_length=200)
+    productImage = models.CharField(max_length=200)
+    productIsContract = models.BooleanField()
+    productIsQuick = models.BooleanField()
