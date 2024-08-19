f02ff6af51a7af22220f738656956afc2be79d11
5hseok <ohhs1010@gmail.com>
2024-08-20 04:43:50 +0900

    [FEAT] Django Model 정의 userType, TagDetail, userTypeTag CT 정의

--- RoomCatcher_Data_AI/report_and_recommand/models.py
+++ RoomCatcher_Data_AI/report_and_recommand/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class DataAnalyzeTagDetail(models.Model):
+    id = models.AutoField(primary_key=True)
+    tag_name = models.CharField(max_length=64, primary_key=True)
+    embedding = models.BinaryField()
+
+    def __str__(self):
+        return self.tag_name
+
+class ReportAndRecommandUserType(models.Model):
+    id = models.AutoField(primary_key=True)
+    type_name = models.CharField(max_length=64)
+    type_explain = models.TextField()
+    embedding = models.BinaryField()
+
+    def __str__(self):
+        return self.type_name
+
+class UserTagCrossTable(models.Model):
+    user_type = models.ForeignKey(ReportAndRecommandUserType, on_delete=models.CASCADE)
+    tag = models.ForeignKey(DataAnalyzeTagDetail, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user_type', 'tag')
